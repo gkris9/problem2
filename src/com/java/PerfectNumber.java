51001b0321966cd25d0e44fc7b0351af497bb823
Gokul Krishna <gkrishna3@sapient.com>
2016-11-29 22:34:11 +0530

    updated perfect number value
    modified code format

--- src/com/java/PerfectNumber.java
+++ src/com/java/PerfectNumber.java
@@ -3,7 +3,7 @@ package com.java;
 import java.util.Scanner;
 
 public class PerfectNumber {
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int value = sc.nextInt();
