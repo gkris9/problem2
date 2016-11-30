e4113b260f7f48a81263f44beacd54e498dfbb9b
Gokul Krishna <gkrishna3@sapient.com>
2016-11-30 12:03:13 +0530

    Refactor
    Added validation for Range

--- src/com/java/PerfectNumberImpl.java
+++ src/com/java/PerfectNumberImpl.java
@@ -1,19 +1,28 @@
 package com.java;
 
 public class PerfectNumberImpl {
+	
+	
 	public boolean verifyPerfectNumber(int number) {
 		boolean flag = false;
+		if(number > 0 && number < 10000)
+		{
+		
 		int tempVar = 0;
 		for (int i = 1; i <= number / 2; i++) {
 			if (number % i == 0) {
 				tempVar += i;
+				}
 			}
-		}
-		if (tempVar == number) {
-			System.out.println("Given number is perfect Number");
-			flag = true;
+			if (tempVar == number) {
+				System.out.println("Given number is perfect Number");
+				flag = true;
+			} else {
+				System.out.println("Given number is not a perfect Number");
+				flag = false;
+			}
 		} else {
-			System.out.println("Given number is not a perfect Number");
+			System.out.println("Given value is out of range");
 			flag = false;
 		}
 
